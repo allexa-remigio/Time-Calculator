9392c1ad4a2e371bf6614e3a2d856b7112f82967
Allexa Remigio <65001005+allexa-remigio@users.noreply.github.com>
2025-11-10 21:30:56 -0800

    Add files via upload

---
+++ Resources/TimeCalculatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  TimeCalculatorApp.swift
+//  TimeCalculator
+//
+//  Created by A R on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeCalculatorApp: App 
+{
+    var body: some Scene 
+    {
+        WindowGroup 
+        {
+            TimeCalcView()
+        }
+    }
+}
